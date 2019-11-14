a4917a16aceb851892c0664849639e0efdadcdd0
Rizki Alif <alifalfarisy73@gmail.com>
2019-11-14 08:47:54 +0700

    Finish Comparison with DBSCAN

--- src/cluster.py
+++ src/cluster.py
@@ -3,13 +3,14 @@ from utils import eucledian_distance
 import collections
 import operator
 from sklearn.cluster import DBSCAN
+from sklearn.model_selection import train_test_split
 import numpy as np
 
 # max(stats.iteritems(), key=operator.itemgetter(1))[0]
 
 #DBSCAN Reference : https://github.com/chrisjmccormick/dbscan/blob/master/dbscan.py
 
-class DBSCAN:
+class MyDBSCAN:
     def __init__(self):
         self.dataset = None
         self.cluster = None
@@ -44,24 +45,8 @@ class DBSCAN:
                             neighbors.append(nn)
             i = i+1
 
-    def create_mapping(self, c):
-        for i in range(1,c+1):
-            counter = {}
-            for j in range(len(self.target)):
-                if self.cluster[j] == i:
-                    value = self.target[j]
-                    if value in counter.keys():
-                        counter[value] = counter[value] + 1
-                    else :
-                        counter[value] = 1
-            max_key = max(counter.items(), key=operator.itemgetter(1))[0]
-            self.map[i] = max_key
-
-
-
-    def fit(self, x, y, eps, minpts):
+    def fit(self, x, eps, minpts):
         self.dataset = x
-        self.target = y
         self.map = {}
         self.eps = eps
         self.minpts = minpts
@@ -80,21 +65,48 @@ class DBSCAN:
                     self.cluster[i] = c
                     self.create_cluster(neighbors, c)
         
-        self.create_mapping(c)
-        print(self.map)
+        # self.create_mapping(c)
+        # print(self.map)
 
     def predict(self, test_data):
-        closest_data = 0
-        min_distance = 9999
-        for i in range(len(self.dataset)):
-            distance = eucledian_distance(self.dataset[i],test_data)
-            if (distance < min_distance and self.cluster[i]!= -1):
-                min_distance = distance
-                closest_data = i
+        result = []
 
-        print(self.cluster[closest_data])
-        return self.map[self.cluster[closest_data]]
+        for td in test_data:
+            closest_data = 0
+            min_distance = 9999
+            for i in range(len(self.dataset)):
+                distance = eucledian_distance(self.dataset[i],td)
+                if (distance < min_distance and self.cluster[i]!= -1):
+                    min_distance = distance
+                    closest_data = i
 
+            result.append(self.cluster[closest_data])
+        return result
+
+def create_dbscan_mapper(target, cluster):
+    cluster_set = set(cluster)
+
+    map = {}
+    for c in cluster_set:
+        counter = {}
+        for j in range(len(target)):
+            if cluster[j] == c:
+                value = target[j]
+                if value in counter.keys():
+                    counter[value] = counter[value] + 1
+                else :
+                    counter[value] = 1
+        max_key = max(counter.items(), key=operator.itemgetter(1))[0]
+        map[c] = max_key
+    return map
+
+def count_accuracy(predict, y_test):
+    correct_count = 0
+    for i in range(len(predict)):
+        if predict[i] == y_test[i]:
+            correct_count = correct_count + 1
+    
+    return correct_count/len(predict)
 
 if __name__ == "__main__" :
     print('tes')
@@ -104,7 +116,24 @@ if __name__ == "__main__" :
     print(iris.target)
     print(iris.target_names)
 
-    dbscan = DBSCAN()
-    dbscan.fit(iris.data, iris.target, 0.41, 3)
-    print(dbscan.predict([5,3,5,2]))
+    mydbscan = MyDBSCAN()
+    scikitdbscan = DBSCAN(eps=0.41, min_samples=3)
+
+    X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.33, random_state=42)
+    mydbscan.fit(X_train, 0.41, 3)
+    scikitdbscan.fit(X_train)
+
+    mydbscan_map = create_dbscan_mapper(y_train,mydbscan.cluster)
+    scikitdbscan_map = create_dbscan_mapper(y_train,scikitdbscan.labels_)
+
+    mydbscan_predict = mydbscan.predict(X_test)
+    scikitdbscan_predict = scikitdbscan.fit_predict(X_test)
+
+    for i in range(len(mydbscan_predict)):
+        mydbscan_predict[i] = mydbscan_map[mydbscan_predict[i]]
+        scikitdbscan_predict[i] = scikitdbscan_map[scikitdbscan_predict[i]]
+
+    print("My DBSCAN accuracy", count_accuracy(mydbscan_predict,y_test), "%")
+    print("Scikit DBSCAN accuracy", count_accuracy(scikitdbscan_predict,y_test), "%")
+    
     # print(dbscan.cluster)
